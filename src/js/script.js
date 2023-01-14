a0b35f6b73f5c764dd08c058a77e6acf4de3cfb0
melkoff <vasamelko@gmail.com>
2023-01-14 14:58:11 +0200

    try scripts

--- src/js/script.js
+++ src/js/script.js
@@ -1,86 +1,34 @@
+// const {name} = require("browser-sync");
 
-// const htmlmin = require("gulp-htmlmin");
+let name = 'John';
+let surname = 'Dou';
+let middleName = 'Patrick';
 
-// let slider = $(document).ready(function () {
-//   $('.your-class').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: true,
-//     dots: true,
-//     infinite: true
-//   });
-// });
+let arraySome = [2, 13, 6, 13, 12.56]
 
-//burger menu
-'use strict';
-const humb = document.querySelector('#burger');
-const humbBody = document.querySelector('#burger-menu');
-const header = document.querySelector('.header');
-const menuOpen = document.querySelector('html');
+const position = arraySome.indexOf("8");
+let someStr = "dadada"
 
+try {
+  Number(someStr)
+} catch {
 
-humb.addEventListener('click', humbHandler);
-
-function humbHandler(e) {
-  e.preventDefault();
-  humbBody.classList.toggle("open");
-  menuOpen.classList.toggle("menu__open");
-  humb.classList.toggle('active');
-  header.classList.toggle('active');
-  window.scrollTo(0, 0);
 }
 
+console.log('Array:', position);
 
-let slider = $(document).ready(function () {
-  $('.teacher').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: false,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    responsive: [
-      {
-        breakpoint: 1145,
-        settings: {
-          slidesToShow: 2,
-        }
-      },{
-        breakpoint: 901,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
+if (position >= 0) {
+  console.log(`Число ${13} є в маисві!`);
+} else {
+  console.log(`Число ${13} не є в маисві!`);
+}
 
-    ]
-  });
-});
+// Array
 
-$(document).ready(function () {
-  $(".feedbacks__slider").slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: false,
-    infinite: true,
-    prevArrow:
-      "<img class='a-left control-c prev slick-prev' src='../img/Vector-left.svg'>",
-    nextArrow:
-      "<img class='a-right control-c next slick-next' src='../img/Vector-right.svg'>",
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          vertical: true,
-          verticalSwiping: true,
-          prevArrow:
-          "<img class='a-left control-b prev slick-prev' src='../img/Vector-left.svg'>",
-        nextArrow:
-          "<img class='a-right control-b next slick-next' src='../img/Vector-down.svg'>",
-        },
-      },
-    ],
-  });
-});
+// let age = [12, 13, 22, 13, 17];
+// const personalData = surname + ' ' + name[0] + '.' + ' ' + middleName[0] + '.';
 
+// console.log('Data: ', name)
+
+// push, unshitf    END and START elements
+// pop, shift     START and END
